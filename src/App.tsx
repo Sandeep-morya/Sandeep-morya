b58af19018f545084fbf0c28fb695de588fbac59
Sandeep-morya <laxmidassmorya@gmail.com>
2023-02-02 02:45:45 -0800

    started

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { ThemeContext } from "./Provider/ThemeContextProvider";
 const App = () => {
 	const { theme, colorMode, toggleColorMode } = useContext(ThemeContext);
 	return (
-		<div style={{ color: theme.c }}>
+		<div style={{ backgroundColor: theme.bg, color: theme.c }}>
 			Hello world <br />
-			<button style={{ backgroundColor: theme.bg }} onClick={toggleColorMode}>
+			<button
+				style={{ backgroundColor: theme.bg, color: theme.c }}
+				onClick={toggleColorMode}>
 				Toggle
 			</button>
 		</div>
