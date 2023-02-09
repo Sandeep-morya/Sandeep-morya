08bb18521df8813942aaf257ce16860c0c593701
Sandeep-morya <laxmidassmorya@gmail.com>
2023-02-09 14:17:32 +0530

    readme icons updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ const App = () => {
 	return (
 		<div style={{ backgroundColor: theme.bg, color: theme.c }}>
 			i will start to create this website after 20 february <br />
+			till then :visit <a href="sandeepmorya.netlify.app"> See my Projects create with vanilla js
+			</a>
 			<button
 				style={{ backgroundColor: theme.bg, color: theme.c }}
 				onClick={toggleColorMode}>
+
 				Toggle
 			</button>
 		</div>
