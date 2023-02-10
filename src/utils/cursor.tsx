41f9f833beb4ccaabad1d67f30e26e8d66657d16
Sandeep-morya <laxmidassmorya@gmail.com>
2023-02-10 23:23:24 +0530

    custom cursor added

---
+++ src/utils/cursor.tsx
@@ -0,0 +1,16 @@
+﻿export const root = document.querySelector("html");
+
+// Real cursor element
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+root?.appendChild(cursor);
+
+// Following extra cursor element
+const follower = document.createElement("div");
+follower.classList.add("cursor", "follower");
+root?.appendChild(follower);
+
+root?.addEventListener("mousemove", (e) => {
+	cursor.style.transform = `translate3d(${e.pageX - 10}px, ${e.pageY}px, 0)`;
+	follower.style.transform = `translate3d(${e.pageX - 10}px, ${e.pageY}px, 0)`;
+});
