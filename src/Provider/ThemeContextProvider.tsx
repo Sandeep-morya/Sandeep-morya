6b2f9f34928a68369e8f0742726ddb8916df1f0b
Sandeep-morya <saabmaurya@gmail.com>
2023-02-19 19:09:36 +0530

    theme reset fixed

--- src/Provider/ThemeContextProvider.tsx
+++ src/Provider/ThemeContextProvider.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+
 export const colorPalettes = {
 	orange_shadow: {
 		main: "rgb(244, 191, 0)",
@@ -31,8 +33,13 @@ let ThemeContext: React.Context<{
 	>;
 }>;
 
+const initialValue:{
+		main: string;
+		dimmed: string;
+	} = JSON.parse(localStorage.getItem('theme') as string) || colorPalettes.orange_shadow;
+
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-	const [color, setColor] = React.useState(colorPalettes.orange_shadow);
+	const [color, setColor] = React.useState(initialValue);
 
 	const theme = {
 		color,
