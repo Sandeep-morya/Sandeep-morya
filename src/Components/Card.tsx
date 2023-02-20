3c88740285abdf1b75cddc5106e5a0ca41d89aff
Sandeep-morya <saabmaurya@gmail.com>
2023-02-20 22:19:42 +0530

    new look given

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -12,17 +12,35 @@ const Card = ({ Icon, title }: Props) => {
 	const { color } = useContext(ThemeContext);
 
 	return (
-		<div
-			className="card"
-			style={{
-				color: color.main,
-				borderColor: color.main,
-				backgroundColor: color.dimmed,
-			}}>
-			<div className="card_icon">
-				<Icon />
+		// <div
+		// 	className="card"
+		// 	style={{
+		// 		color: color.main,
+		// 		borderColor: color.main,
+		// 		backgroundColor: color.dimmed,
+		// 	}}>
+		// 	<div className="card_icon">
+		// 		<Icon />
+		// 	</div>
+		// 	<div className="card_text">{title}</div>
+		// </div>
+		<div className="container">
+			<div className="front" style={{ background: color.main }}>
+				<div className="inner">
+					<div>
+						<Icon size={80} color={color.main} />
+					</div>
+					<span>{title}</span>
+				</div>
 			</div>
-			<div className="card_text">{title}</div>
+			<div className="back" style={{ background:"url(https://unsplash.it/502/502/)" }}>
+				<div className="inner">
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum
+						repellat velit quae suscipit c.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
