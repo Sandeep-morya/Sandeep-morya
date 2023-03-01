869168d84dfdacbf09571ac973a7b0bdba0bac0c
Sandeep-morya <saabmaurya@gmail.com>
2023-03-01 17:00:27 +0530

    minor tweaks

--- src/Components/Avatar.tsx
+++ src/Components/Avatar.tsx
@@ -3,21 +3,17 @@ import "../Styles/avatar.css";
 import { useContext } from "react";
 import { ThemeContext } from "../Provider/ThemeContextProvider";
 
-
-
 const Avatar = () => {
 	const { color } = useContext(ThemeContext);
 	return (
-
-			<div
-				className="avatar"
-				style={{
-					backgroundColor: color.main,
-					borderRadius: "50%",
-				}}>
-				<img style={{ outlineColor: color.main }} src="/vector.png" alt="" />
-			</div>
-
+		<div
+			className="avatar"
+			style={{
+				backgroundColor: color.main,
+				borderRadius: "50%",
+			}}>
+			<img style={{ outlineColor: color.main }} src="/vector.png" alt="" />
+		</div>
 	);
 };
 
